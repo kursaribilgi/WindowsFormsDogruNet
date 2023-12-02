acc83810583cd2d89c69f516aef949e5fa6368a8
muratapk <muratapk@hotmail.com>
2023-12-02 11:20:30 +0300

    Add project files.

---
+++ WindowsFormsApp7/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace WindowsFormsApp7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtTrDogru = new System.Windows.Forms.TextBox();
+            this.txtTrYanlis = new System.Windows.Forms.TextBox();
+            this.txtTrNet = new System.Windows.Forms.TextBox();
+            this.txtMtDogru = new System.Windows.Forms.TextBox();
+            this.txtMtYanlis = new System.Windows.Forms.TextBox();
+            this.txtMtNet = new System.Windows.Forms.TextBox();
+            this.txtFzDogru = new System.Windows.Forms.TextBox();
+            this.txtFzYanlis = new System.Windows.Forms.TextBox();
+            this.txtFzNet = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(69, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Türkçe";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(69, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 16);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Matematik";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(69, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 16);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Fizik";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(221, 47);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 16);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Doğru";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(318, 47);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 16);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Yanlış";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(411, 47);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(28, 16);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Net";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(221, 211);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(44, 16);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "label1";
+            // 
+            // txtTrDogru
+            // 
+            this.txtTrDogru.Location = new System.Drawing.Point(224, 82);
+            this.txtTrDogru.Name = "txtTrDogru";
+            this.txtTrDogru.Size = new System.Drawing.Size(56, 22);
+            this.txtTrDogru.TabIndex = 1;
+            // 
+            // txtTrYanlis
+            // 
+            this.txtTrYanlis.Location = new System.Drawing.Point(306, 82);
+            this.txtTrYanlis.Name = "txtTrYanlis";
+            this.txtTrYanlis.Size = new System.Drawing.Size(56, 22);
+            this.txtTrYanlis.TabIndex = 1;
+            // 
+            // txtTrNet
+            // 
+            this.txtTrNet.Location = new System.Drawing.Point(393, 82);
+            this.txtTrNet.Name = "txtTrNet";
+            this.txtTrNet.Size = new System.Drawing.Size(56, 22);
+            this.txtTrNet.TabIndex = 1;
+            // 
+            // txtMtDogru
+            // 
+            this.txtMtDogru.Location = new System.Drawing.Point(224, 124);
+            this.txtMtDogru.Name = "txtMtDogru";
+            this.txtMtDogru.Size = new System.Drawing.Size(56, 22);
+            this.txtMtDogru.TabIndex = 1;
+            // 
+            // txtMtYanlis
+            // 
+            this.txtMtYanlis.Location = new System.Drawing.Point(306, 124);
+            this.txtMtYanlis.Name = "txtMtYanlis";
+            this.txtMtYanlis.Size = new System.Drawing.Size(56, 22);
+            this.txtMtYanlis.TabIndex = 1;
+            // 
+            // txtMtNet
+            // 
+            this.txtMtNet.Location = new System.Drawing.Point(393, 124);
+            this.txtMtNet.Name = "txtMtNet";
+            this.txtMtNet.Size = new System.Drawing.Size(56, 22);
+            this.txtMtNet.TabIndex = 1;
+            // 
+            // txtFzDogru
+            // 
+            this.txtFzDogru.Location = new System.Drawing.Point(224, 161);
+            this.txtFzDogru.Name = "txtFzDogru";
+            this.txtFzDogru.Size = new System.Drawing.Size(56, 22);
+            this.txtFzDogru.TabIndex = 1;
+            // 
+            // txtFzYanlis
+            // 
+            this.txtFzYanlis.Location = new System.Drawing.Point(306, 161);
+            this.txtFzYanlis.Name = "txtFzYanlis";
+            this.txtFzYanlis.Size = new System.Drawing.Size(56, 22);
+            this.txtFzYanlis.TabIndex = 1;
+            // 
+            // txtFzNet
+            // 
+            this.txtFzNet.Location = new System.Drawing.Point(393, 161);
+            this.txtFzNet.Name = "txtFzNet";
+            this.txtFzNet.Size = new System.Drawing.Size(56, 22);
+            this.txtFzNet.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(321, 203);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 33);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "HESAPLA";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtFzNet);
+            this.Controls.Add(this.txtFzYanlis);
+            this.Controls.Add(this.txtFzDogru);
+            this.Controls.Add(this.txtMtNet);
+            this.Controls.Add(this.txtMtYanlis);
+            this.Controls.Add(this.txtMtDogru);
+            this.Controls.Add(this.txtTrNet);
+            this.Controls.Add(this.txtTrYanlis);
+            this.Controls.Add(this.txtTrDogru);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtTrDogru;
+        private System.Windows.Forms.TextBox txtTrYanlis;
+        private System.Windows.Forms.TextBox txtTrNet;
+        private System.Windows.Forms.TextBox txtMtDogru;
+        private System.Windows.Forms.TextBox txtMtYanlis;
+        private System.Windows.Forms.TextBox txtMtNet;
+        private System.Windows.Forms.TextBox txtFzDogru;
+        private System.Windows.Forms.TextBox txtFzYanlis;
+        private System.Windows.Forms.TextBox txtFzNet;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsFormsApp7/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        string netim(string dogru, string yanlis)
+        {
+
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+
+            int sonuc = 0; 
+            if(dogru!=null && yanlis!=null)
+            {
+                 sonuc = Convert.ToInt16(dogru) - Convert.ToInt16(yanlis);
+                return sonuc.ToString();
+            }
+            else
+            {
+                return sonuc.ToString();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtTrNet.Text = netim(txtTrDogru.Text, txtTrYanlis.Text);
+            txtMtNet.Text = netim(txtMtDogru.Text, txtMtYanlis.Text);
+            txtFzNet.Text = netim(txtFzDogru.Text, txtFzYanlis.Text);
+        }
+    }
+}
